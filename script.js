ddab07b7bed009b872ed5bbab25f04fb6d256187
Volkan <132361922+vlk34@users.noreply.github.com>
2023-08-24 18:18:07 +0300

    Added gallery, made it responsive
    Gallery, About us and Contact is all done and responsive.

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const btnNavEl = document.querySelector(".btn-mobile-nav");
 const headerEl = document.querySelector(".header");
-const mainNav = document.querySelector(".main-nav");
 
 btnNavEl.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
